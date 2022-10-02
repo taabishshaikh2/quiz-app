18331a66b26db5cc63d53aabb583f8377968b234
Taabish Shaikh <taabishshaikh2@gmail.com>
2022-10-02 14:25:03 +0530

    quiz app created using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import { data } from "./questionData";
 
 function App() {
+  const [quest, setQuest] = useState(0);
+  const [score, setScore] = useState(0)
+  const [displayScore, setDisplayScore] = useState(false)
+  
+  
+  const handleClick = (e) => {
+    const nxtQuest = quest + 1;
+    // const updateScore = score + 1
+    const value = e.target.value;
+    if (nxtQuest < data.length ) {
+      setQuest(nxtQuest);
+
+    }
+    else{
+      setDisplayScore(true)
+    }
+
+    if (data[quest].correct === value) {
+      
+      setScore(score + 1)
+      
+      console.log(true+" score is: "+ (score + 1));
+
+    } else {
+      console.log(false);
+    }
+    
+    // let var = quest < data.length
+    
+    console.log("clicked", quest);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quiz-container">
+        Here will be the questions
+
+        <div className="quest_container">
+          {displayScore ?
+          
+          <h1 className="end_txt">You've reached the end, your score is {score}</h1>
+          :
+          <>
+          <div className="qts_opts">
+          {data[quest].question}
+          </div>
+          <div className="options">
+            {data[quest].options.map((ans) => (
+              // <input onClick={handleClick}  className="ans_txt" type="button" value={ans.answer} />
+              <button onClick={handleClick}  className="ans_txt" type="button" value={ans.answer}>{ans.answer}</button>
+            ))}
+          </div>
+          
+          </>
+          
+          }
+          
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/questionData.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "What is the Captial of India",
+        options: [
+            {answer: 'Delhi',},
+            {answer: 'Mumbai',},
+            {answer: 'Jaipur',},
+            {answer: 'Chennai',},
+        ],
+        correct: 'Delhi'
+    },
+    {
+        question: "What is the National Fruit of India",
+        options: [
+            {answer: 'Grape',},
+            {answer: 'Orange',},
+            {answer: 'Mango'},
+            {answer: 'Apple',},
+        ],
+        correct: 'Mango'
+    },
+    {
+        question: "India’s largest museum is located at ",
+        options: [
+            {answer: 'Delhi',},
+            {answer: 'Kolkata'},
+            {answer: 'Jaipur',},
+            {answer: 'Chennai',},
+        ],
+        correct: 'Kolkata'
+    },
+    {
+        question: "Who wrote 'Discovery of India'?",
+        options: [
+            {answer: 'Bal Gangadhar Tilak',},
+            {answer: 'Mahatma Gandhi',},
+            {answer: 'Jawaharlal Nehru'},
+            {answer: 'APJ Abdul Kalam',},
+        ],
+        correct: 'Jawaharlal Nehru'
+    }
+]
